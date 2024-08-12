17bd6aaecdb689d8f9b4a656e5cbc148b90922d2
DESKTOP-HN9D76N\Ichsan Ilyasa <ichsanilyasa3@gmail.com>
2024-08-12 16:37:03 +0700

    update 10 navbar etc

--- app/src/main/java/com/example/uxlectureproj/HomeActivity.java
+++ app/src/main/java/com/example/uxlectureproj/HomeActivity.java
@@ -7,19 +7,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.uxlectureproj.databinding.ActivityHomeBinding;
+import com.example.uxlectureproj.databinding.ActivityMainBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        replaceFragment(new ExploreFragment());
+
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.BottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.explore) {
+                replaceFragment(new ExploreFragment());
+            } else if (itemId == R.id.find) {
+                replaceFragment(new FindFragment());
+            } else if (itemId == R.id.friends) {
+                replaceFragment(new FriendsFragment());
+            } else if (itemId == R.id.events) {
+                replaceFragment(new EventFragment());
+            } else if (itemId == R.id.me) {
+                replaceFragment(new MeFragment());
+            }
+
+            return true;
         });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
